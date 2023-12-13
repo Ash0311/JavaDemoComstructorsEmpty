f4b63ab7211f2d8f4b8fbef25e83a306a9593df4
ash0311 <s0u1sb4n3.dev@gmail.com>
2023-12-13 13:58:50 -0600

    Initial commit

---
+++ src/main/java/vet/ashleyking/GreetingClass.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class GreetingClass {
+
+    // Empty constructor
+    public GreetingClass() {
+        // Nothing to do here
+    }
+
+    // Method to display a greeting with the user's name
+    public void displayGreeting() {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompting the user for their name
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Displaying the greeting message
+        System.out.println("Hello, " + name + "!");
+
+        scanner.close();
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        // Creating an instance of GreetingClass using the empty constructor
+        GreetingClass greeting = new GreetingClass();
+
+        // Calling the displayGreeting method
+        greeting.displayGreeting();
+    }
+}
